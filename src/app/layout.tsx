fd1170136e2bb6991b8b02fee7b8014f3acb37e6
Arnaud Delante <contact@arnaud.ninja>
2023-11-22 00:36:52 +0100

    Change padding of the main box

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${GeistSans.variable} bg-gray-100 py-16`}>
-        <div className="flex items-center justify-center lg:h-screen">
+      <body className={`font-sans ${GeistSans.variable} bg-gray-100 py-6`}>
+        <div className="flex items-start justify-center">
           <div className="w-11/12 rounded-lg bg-white p-6 shadow-sm sm:w-11/12 md:w-8/12 lg:w-6/12">
             <div className="flex w-full items-center justify-between p-3">
               <h1 className="text-xl font-semibold">OAOS deploys history</h1>
