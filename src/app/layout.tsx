e22e4484b4b48aa98ae680ecea367ec8c7692045
Arnaud Delante <contact@arnaud.ninja>
2023-11-21 15:59:27 +0100

    Use Geist as font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "~/styles/globals.css";
 
+import { GeistSans } from "geist/font/sans";
+
 export const metadata = {
   title: "OAOS deploys history",
   description: "Historique des différents déploiements de OAOS",
@@ -13,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`bg-gray-100 py-16 font-sans`}>
+      <body className={`font-sans ${GeistSans.variable} bg-gray-100 py-16`}>
         <div className="flex items-center justify-center lg:h-screen">
           <div className="w-11/12 rounded-lg bg-white p-6 shadow-sm sm:w-11/12 md:w-8/12 lg:w-6/12">
             <div className="flex w-full items-center justify-between p-3">
