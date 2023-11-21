04c0669a3b4b11922c11067901e8cca18a10f82f
Arnaud Delante <contact@arnaud.ninja>
2023-11-21 15:42:24 +0100

    Remove Inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,5 @@
 import "~/styles/globals.css";
 
-import { Inter } from "next/font/google";
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
 export const metadata = {
   title: "OAOS deploys history",
   description: "Historique des différents déploiements de OAOS",
@@ -20,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable} bg-gray-100 py-16`}>
+      <body className={`bg-gray-100 py-16 font-sans`}>
         <div className="flex items-center justify-center lg:h-screen">
           <div className="w-11/12 rounded-lg bg-white p-6 shadow-sm sm:w-11/12 md:w-8/12 lg:w-6/12">
             <div className="flex w-full items-center justify-between p-3">
