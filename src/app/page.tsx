7c1ba8d5f42cb5802d8ad40c48224c56e892b205
Arnaud Delante <contact@arnaud.ninja>
2023-11-21 23:09:46 +0100

    Comment the call to deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,29 +13,29 @@ interface DeploysProps {
   };
 }
 
-export default async function HomePage() {
-  const deploys = (await fetch(`${env.NEXT_PUBLIC_BASE_URL}/api/deploy`, {
-    next: {
-      revalidate: 600,
-    },
-  })
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      throw new Error("Something went wrong");
-    })
-    .catch((error) => {
-      console.log(error);
-    })) as DeploysProps;
+export default function HomePage() {
+  // const deploys = (await fetch(`${env.NEXT_PUBLIC_BASE_URL}/api/deploy`, {
+  //   next: {
+  //     revalidate: 600,
+  //   },
+  // })
+  //   .then((res) => {
+  //     if (res.ok) {
+  //       return res.json();
+  //     }
+  //     throw new Error("Something went wrong");
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   })) as DeploysProps;
 
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2">
-      <Card name={"PRD"} deploys={deploys.deploys.PRD} />
-      <Card name={"UAT"} deploys={deploys.deploys.UAT} />
-      <Card name={"U1AT"} deploys={deploys.deploys.U1AT} />
-      <Card name={"U2AT"} deploys={deploys.deploys.U2AT} />
-      <Card name={"U3AT"} deploys={deploys.deploys.U3AT} />
+      {/*<Card name={"PRD"} deploys={deploys.deploys.PRD} />*/}
+      {/*<Card name={"UAT"} deploys={deploys.deploys.UAT} />*/}
+      {/*<Card name={"U1AT"} deploys={deploys.deploys.U1AT} />*/}
+      {/*<Card name={"U2AT"} deploys={deploys.deploys.U2AT} />*/}
+      {/*<Card name={"U3AT"} deploys={deploys.deploys.U3AT} />*/}
     </div>
   );
 }
